18b4fef9f93a1e8c92edc8d3cc25fd0315e3f8c4
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2024-11-26 00:39:29 -0300

    feat: add margin-top cta

--- src/components/sections/Cta.tsx
+++ src/components/sections/Cta.tsx
@@ -6,8 +6,8 @@ import BlurIn from '../ui/blur-in';
 
 export const Cta = () => {
   return (
-    <AnimatedSection className='container-grid pb-24 pt-40 flex flex-col items-center'>
-      <BlurIn word="Links fracos limitam seu alcace" className='text-4xl sm:text-5xl font-semibold tracking-tight text-center md:text-7xl max-w-2xl'></BlurIn>
+    <AnimatedSection className='container-grid pb-24 pt-44 flex flex-col items-center'>
+      <BlurIn word="Links fracos limitam seu alcace" className='text-4xl sm:text-5xl font-medium tracking-tight text-center md:text-7xl max-w-2xl'></BlurIn>
       <BlurIn word="Links comuns podem ser o elo fraco da sua estratégia. Zaplinker reforça suas conexões digitais, maximizando seu alcance e impulsionando resultados mensuráveis." className="text-muted-foreground max-w-2xl sm:text-lg tracking-tight my-6 text-center"></BlurIn>
       <div className='flex gap-4'>
         <Link
