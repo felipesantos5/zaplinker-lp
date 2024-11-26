58ffca298894d129e079d97b87c7f1ddc5db12dc
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2024-11-26 00:26:13 -0300

    feat: add metatag

--- src/components/sections/Cta.tsx
+++ src/components/sections/Cta.tsx
@@ -6,8 +6,8 @@ import BlurIn from '../ui/blur-in';
 
 export const Cta = () => {
   return (
-    <AnimatedSection className='container-grid pb-24 pt-36 flex flex-col items-center'>
-      <BlurIn word="Amplifique Seu Alcance: Links WhatsApp Personalizados para Conexões Poderosas" className='text-4xl sm:text-5xl font-medium tracking-tight text-center md:text-7xl max-w-4xl'></BlurIn>
+    <AnimatedSection className='container-grid pb-24 pt-48 flex flex-col items-center'>
+      <BlurIn word="Links fracos limitam seu alcace" className='text-4xl sm:text-5xl font-medium tracking-tight text-center md:text-7xl max-w-2xl'></BlurIn>
       {/* text-4xl sm:text-5xl font-medium tracking-tight text-center md:text-7xl max-w-4xl */}
       <BlurIn word="Otimize seu atendimento e aumente suas conversões com links personalizados e QR codes prontos para ação. Torne cada interação mais eficiente e acessível." className="text-muted-foreground max-w-2xl sm:text-lg tracking-tight my-6 text-center"></BlurIn>
       {/* text-muted-foreground max-w-2xl sm:text-lg tracking-tight my-6 text-center */}
@@ -21,7 +21,7 @@ export const Cta = () => {
           duration={2200}
         >
           <ShinyButton className='z-10'>
-            Teste gratis
+            Teste grátis
           </ShinyButton>
         </Link>
 
